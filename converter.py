b76bce14bd368682b22983c8a52baf8e1f0bfc2e
Saddam <saddam.khadra@digitalberry.fr>
2023-04-22 15:54:43 +0100

    imporvement

--- converter.py
+++ converter.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import HTTPException
 from num2words import num2words, CONVERTER_CLASSES
 from pydantic import BaseModel
@@ -11,6 +12,9 @@ class Item(BaseModel):
     separator: str | None = None
     decimal: int | None = None
     language: str
+
+    class Config:
+        smart_union = True
 
 
 def validate_input_data(item: Item):
@@ -28,49 +32,48 @@ def validate_input_data(item: Item):
     return item
 
 
-def convert_to_currency(
-        number: int | float,
-        language: str,
-        decimal: int = None,
-        delete_from_sentence: str = None,
-        currency: str = None,
-        decimal_currency: str = None,
-        separator: str = None):
-    if isinstance(number, float):
-        number_str = f"{number:.{decimal}f}" if decimal is not None else str(number)
-        print(number_str)
-        print(type(number_str))
-        num_list = number_str.split(".")
-        part1 = num2words(int(num_list[0]), lang=language)
-        part2 = num2words(int(num_list[1]), lang=language)
-        if delete_from_sentence is not None:
-            part1_list = part1.split(" ")
-            part1_list = [i for i in part1_list if i != delete_from_sentence]
-            part1 = " ".join(part1_list)
-            part2_list = part2.split(" ")
-            part2_list = [i for i in part2_list if i != delete_from_sentence]
-            part2 = " ".join(part2_list)
-        if currency is not None and decimal_currency is not None:
-            return (
-                f"{part1} {currency} {separator} {part2} {decimal_currency}".replace(
-                    ',', ''
-                )
-                if separator is not None
-                else f"{part1} {currency} {part2} {decimal_currency}".replace(
-                    ',', ''
-                )
+def convert_to_currency(item: Item) -> str:
+    if isinstance(item.number, float):
+        return when_number_is_float(item=item)
+    elif isinstance(item.number, int):
+        return when_number_is_int(item=item)
+
+
+def when_number_is_int(item: Item) -> str:
+    result = num2words(number=item.number, lang=item.language)
+    if item.delete_from_sentence is not None:
+        result_list = result.split(" ")
+        result_list = [i for i in result_list if i != item.delete_from_sentence]
+        result = " ".join(result_list)
+    if item.currency is not None:
+        return f"{result} {item.currency}".replace(',', '')
+    else:
+        return f"{result}".replace(',', '')
+
+
+def when_number_is_float(item: Item) -> str:
+    number_str = f"{item.number:.{item.decimal}f}" if item.decimal is not None else str(item.number)
+    num_list = number_str.split(".")
+    part1 = num2words(int(num_list[0]), lang=item.language)
+    part2 = num2words(int(num_list[1]), lang=item.language)
+    if item.delete_from_sentence is not None:
+        part1_list = part1.split(" ")
+        part1_list = [i for i in part1_list if i != item.delete_from_sentence]
+        part1 = " ".join(part1_list)
+        part2_list = part2.split(" ")
+        part2_list = [i for i in part2_list if i != item.delete_from_sentence]
+        part2 = " ".join(part2_list)
+    if item.currency is not None and item.decimal_currency is not None:
+        return (
+            f"{part1} {item.currency} {item.separator} {part2} {item.decimal_currency}".replace(
+                ',', ''
             )
-        if separator is not None:
-            return f"{part1} {separator} {part2}".replace(',', '')
-        else:
-            return f"{part1} {part2}".replace(',', '')
-    elif isinstance(number, int):
-        result = num2words(number=number, lang=language)
-        if delete_from_sentence is not None:
-            result_list = result.split(" ")
-            result_list = [i for i in result_list if i != delete_from_sentence]
-            result = " ".join(result_list)
-        if currency is not None:
-            return f"{result} {currency}".replace(',', '')
-        else:
-            return f"{result}".replace(',', '')
+            if item.separator is not None
+            else f"{part1} {item.currency} {part2} {item.decimal_currency}".replace(
+                ',', ''
+            )
+        )
+    if item.separator is not None:
+        return f"{part1} {item.separator} {part2}".replace(',', '')
+    else:
+        return f"{part1} {part2}".replace(',', '')
--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException
-from num2words import CONVERTER_CLASSES
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends
 from converter import convert_to_currency, Item, validate_input_data
 
 app = FastAPI()
 
 
-
-
-
-
 @app.post("/api/converter/")
 async def converter(item: Item = Depends(validate_input_data)):
-    return {"message": convert_to_currency(
-        number=item.number,
-        language=item.language,
-        decimal=item.decimal,
-        delete_from_sentence=item.delete_from_sentence,
-        currency=item.currency,
-        decimal_currency=item.decimal_currency,
-        separator=item.separator
-    )}
+    return {"message": convert_to_currency(item=item)}
